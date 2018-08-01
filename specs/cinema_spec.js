9cac07f7171cd5ae63887058b106ebdea0dfc784
StuartCharters <charters.stuart@gmail.com>
2018-08-01 11:52:07 +0100

    can get titles of films for a cinema

--- specs/cinema_spec.js
+++ specs/cinema_spec.js
@@ -28,8 +28,9 @@ describe('Cinema', function () {
     assert.deepStrictEqual(actual, films);
   });
 
-  xit('should be able to get a list of film titles', function () {
-
+  it('should be able to get a list of film titles', function () {
+    const results = cinema.getFilmTitles();
+    assert.deepStrictEqual(results, [`Moonlight`, 'Blade Runner 2049', 'Dunkirk', 'Black Panther', 'T2 Trainspotting']);
   });
   xit('should be able to find a film by title');
   xit('should be able to filter films by genre');
